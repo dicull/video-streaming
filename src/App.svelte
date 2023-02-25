306c73c31f0dc8f2544469fabb01e0b5c6818433
kwon <fides4u@gmail.com>
2023-02-25 11:37:14 +0900

    chore: 중간커밋

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,73 @@
 <script>
-import Counter from './lib/Counter.svelte';
+	import Counter from './lib/Counter.svelte';
+
+	let inputFile;
+
+	const onFileChange = (e) => {
+		
+	};
+
+	const handleUpload = (e) => {
+		inputFile.click();
+	};
 </script>
 
-<header><h1>Video Streaming</h1></header>
-<main>
-	<div class="card">
+<header class="position-fixed top-0 start-0 end-0">
+	<h2 class="title m-3 text-start">Video Streaming</h2>
+</header>
+<main class="position-absolute top-0 bottom-0 start-0 end-0 text-center">
+	<div class="content m-3">
+		<div class="mb-3">
+			<input
+				type="file"
+				hidden
+				bind:this="{inputFile}"
+				on:change="{onFileChange}" />
+			<button
+				type="button"
+				class="btn btn-outline-secondary"
+				on:click="{handleUpload}">Video File Upload</button>
+		</div>
+		<div>또는</div>
+		<div class="input-group mb-3">
+			<input
+				type="text"
+				class="form-control"
+				placeholder=".m3u8 file url 을 입력" />
+			<button class="btn btn-outline-secondary" type="button"
+				>확인</button>
+		</div>
 		<Counter />
 	</div>
 </main>
-<footer>
-	<p class="read-the-docs">Vite + Svelte</p>
+<footer class="position-fixed bottom-0 start-0 end-0 text-center">
+	<p class="read-the-docs">
+		Create with lambda + svelte + bootstrap + sass + vite
+	</p>
 </footer>
 
-<style>
-.read-the-docs {
-	color: #888;
-}
+<style lang="scss">
+	header {
+		z-index: 1;
+		background-color: #8da9c4;
+	}
+
+	main {
+		z-index: 0;
+		background-color: #eef4ed;
+		height: 100vh;
+
+		.content {
+			position: relative;
+			top: 100px;
+		}
+	}
+
+	footer {
+		z-index: 1;
+	}
+
+	.read-the-docs {
+		color: #888;
+	}
 </style>
